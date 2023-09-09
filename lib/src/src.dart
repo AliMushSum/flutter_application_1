8c07625d2b1f87f027fc74efea8f7ae7707935d2
AliMushSum <usmonodilov181@gmail.com>
2023-09-09 19:26:09 +0500

    add new file

---
+++ lib/src/src.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SrvWid extends StatefulWidget {
+  const SrvWid({super.key});
+
+  @override
+  State<SrvWid> createState() => _SrvWidState();
+}
+
+class _SrvWidState extends State<SrvWid> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
